b01cf5580c2f1b61e6cb4c1e57cab4aad059c5c1
Windows 11 Pro <abdulbositmm>
2022-04-05 10:04:55 +0500

    yangi

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
-import 'dart:html';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 class HomePage extends StatefulWidget {
-  static final String id = "home_page";
+  static final String id = 'hamepage';
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -12,10 +12,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello PDP online"),
-      )
-    );
-  }
+     body:Center(
+       child:Column(
+         children: [
+           Container(
+             color: Colors.pink,
+           )
+         ],
+       )
+     ),
+     );
 }
-
+}
