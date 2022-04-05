2db0aefd66049e16e71ce9b1d2751706238b9851
Windows 11 Pro <abdulbositmm>
2022-04-05 10:18:26 +0500

    task3 tugalanmagan

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,15 +12,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body:Center(
-       child:Column(
-         children: [
-           Container(
-             color: Colors.pink,
-           )
-         ],
-       )
-     ),
+      body: Container(
+        height: ,
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              height: 600,
+              color: Colors.pink,
+            )
+          ],
+        ),
+      )
      );
 }
 }
